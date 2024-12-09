a9f26882df7f65d78b1b2d378c2c941277330d31
lilia <lilia@DESKTOP-64J80RS>
2024-12-09 19:24:15 +0200

    Добавьте файлы проекта.

---
+++ Homework13(course).cpp
@@ -0,0 +1,44 @@
+﻿/*1)
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    char field[3][3];
+
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cout << field[i][j] << " ";  
+        }
+        cout << endl;  
+    }
+
+    return 0;
+}
+*/
+/*2)
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int field[3][3];
+
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            field[i][j] = 0;  
+        }
+    }
+
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << field[i][j] << " ";  
+        }
+        cout << endl; 
+    }
+
+    return 0;
+}
+*/
